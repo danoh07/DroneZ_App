96ecee943553d1b3e3d7aa5fa5eb937e5068d494
danoh07 <danoh@bu.edu>
2023-03-12 23:05:52 -0400

    made basic wireframe and working on joy stick

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Drone Z',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Drone Z Home Page'),
     );
@@ -33,84 +24,244 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'DroneZ Flight Controller',
+              style: TextStyle(fontSize: 30),
+            ),
+            Container(
+              margin: EdgeInsets.all(40),
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey[100],
+                shape: BoxShape.circle,
+              ),
+              child:
+                  Center(child: Text('LOGO', style: TextStyle(fontSize: 50))),
+            ),
+            SizedBox(
+              height: 100,
+              width: 320,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.bookmark,
+                  size: 50,
+                ),
+                label: Text('See Past Flights', style: TextStyle(fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                    foregroundColor: Colors.black),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 100,
+              width: 320,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MenuPage()));
+                },
+                icon: Icon(
+                  Icons.rocket,
+                  size: 50,
+                ),
+                label: Text(
+                  'Take Flight',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                    foregroundColor: Colors.black),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 100,
+              width: 320,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.question_mark,
+                  size: 50,
+                ),
+                label: Text('lorum ipsum', style: TextStyle(fontSize: 20)),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[100],
+                    foregroundColor: Colors.black),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MenuPage extends StatefulWidget {
+  @override
+  State<MenuPage> createState() => _MenuPage();
+}
+
+class _MenuPage extends State<MenuPage> {
+  var selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+
+    switch (selectedIndex) {
+      case 0:
+        page = Placeholder();
+        break;
+      case 1:
+        page = TakeFlightPage();
+        break;
+      case 2:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    var mainArea = AnimatedSwitcher(
+      duration: Duration(milliseconds: 200),
+      child: page,
+    );
+
+    return Scaffold(
+      body: mainArea,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark), label: 'Past Flight'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.rocket), label: 'Take Flight'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.question_mark), label: 'lorum ipsum'),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TakeFlightPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Select your', style: TextStyle(fontSize: 35)),
+          Text('piloting mode', style: TextStyle(fontSize: 35)),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('so many choices...', style: TextStyle(fontSize: 17)),
+          ),
+          SizedBox(height: 50),
+          FlightControlOptionCard(),
+        ],
+      ),
+    );
+  }
+}
+
+class FlightControlOptionCard extends StatelessWidget {
+  const FlightControlOptionCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: 310,
+            height: 360,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey[50],
+              borderRadius: BorderRadius.circular(3),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 3,
+                  offset: Offset(0, 6), // Shadow position
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 230,
+                height: 80,
+                child: ElevatedButton(
+                    onPressed: () {
+                      print('Automatic');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3.0)),
+                      backgroundColor: Colors.blueGrey[100],
+                    ),
+                    child: Text(
+                      'AUTOMATIC',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),
+                    )),
+              ),
+              SizedBox(height: 75),
+              SizedBox(
+                width: 230,
+                height: 80,
+                child: ElevatedButton(
+                    onPressed: () {
+                      print('Manual');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3.0)),
+                      backgroundColor: Colors.blueGrey[100],
+                    ),
+                    child: Text(
+                      'MANUAL',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),
+                    )),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
